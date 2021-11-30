2662b4afd815bb5b4499423114a245ce48a98411
DESKTOP-4EFEVVD\jrsar <jr.sarcilla07@gmail.com>
2021-11-30 19:05:20 +0800

    Arrange items

--- src/components/Main.js
+++ src/components/Main.js
@@ -28,25 +28,25 @@ const Main = () => {
                 
                 <div className="relative flex flex-row flex-wrap gap-x-2 items-center pb-1/4">
                     <div className="relative flex-auto pb-11/12">
-                        <img src="/assets/app_login.png" className="absolute top-0 w-10/12 h-full object-cover rounded-xl" alt="Daietto - App Login" />
+                        <img src="/assets/app_login.png" className="absolute top-0 left-1/12 w-3/4 h-full object-cover rounded-xl" alt="Daietto - App Login" />
                     </div>
                     <div className="relative flex-auto pb-11/12">
-                        <img src="/assets/app_homescreen.png" className="absolute top-3/12 w-10/12 h-full object-cover rounded-xl" alt="Daietto - App Login" />
+                        <img src="/assets/app_homescreen.png" className="absolute top-3/12 left-1/12 w-10/12 h-full object-cover rounded-xl" alt="Daietto - App Login" />
                     </div>
 
-                    <div className="flex flex-row flex-wrap content-start justify-start absolute bottom-0 left-0 w-full h-full">
+                    {/* <div className="flex flex-row flex-wrap content-start justify-start absolute bottom-0 left-0 w-full h-full">
                         <img src="/assets/app_results.svg" className="app_results absolute w-full h-full" alt="Daietto - App Results"/>
-                    </div>
+                    </div> */}
                 </div>
            </section>
            
            {/* Feature 1 */}
-           <section>
+           <section className="z-10">
                 <div>
                     <img src="/assets/top_divider.svg" alt="Daeitto - Top divider" />
                     <h2 className="text-accent text-6xl font-bold">
-                        <strong className="text-primary">Descubre </strong>
-                        que tan fácil es crear alimentos saludables
+                        <strong className="text-primary">Discover </strong>
+                        how easy it is to create healthy foods
                     </h2>
                 </div>
            </section>
