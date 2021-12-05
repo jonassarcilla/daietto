c4bd2616c51a25a145c95babeea6d4c0ba9ee18a
DESKTOP-4EFEVVD\jrsar <jr.sarcilla07@gmail.com>
2021-12-05 17:18:49 +0800

    add viewport

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ export default function Home() {
       <Head>
         <title>Daeitto</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       </Head>
 
       <div className="bg-secondary max-w-screen-sm mx-auto">
