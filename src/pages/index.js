3f401ef2474afef5719edd307674a64e0b1545eb
DESKTOP-4EFEVVD\jrsar <jr.sarcilla07@gmail.com>
2021-12-05 18:09:04 +0800

    Set to mobile only

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       </Head>
 
-      <div className="bg-secondary max-w-screen-sm mx-auto">
+      <div className="bg-secondary max-w-smartphone mx-auto">
         <Header/>
         <Main/>
         <Footer/>
